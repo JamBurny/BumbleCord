70007beae0b1705eadef429ff301501432ca5248
James Burnet <jburnet@student.unimelb.edu.au>
2024-04-08 19:58:51 +1000

    added sample command

--- index.ts
+++ index.ts
@@ -1,2 +1,25 @@
-console.log("Hello via Bun!");
-console.log(process.env.TEST);
+import { REST, Routes } from "discord.js";
+
+const SECRET: string = process.env.SECRET as string;
+const CLIENT_ID: string = process.env.CLIENT_ID as string;
+
+const commands = [
+    {
+        name: "ping",
+        description: "Replies with Pong!",
+    },
+];
+
+const rest = new REST({ version: "10" }).setToken(SECRET);
+
+try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), {
+        body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+    console.error(error);
+}
